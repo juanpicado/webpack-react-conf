1ba6f17f2935a177d207c055aa202c954f8fd3c9
Juan Carlos Picado <juan@encuestame.org>
2016-07-31 09:08:31 +0200

    improve loaders

--- README.md
+++ README.md
@@ -3,6 +3,16 @@
 This module helps to reduce the overloaded webpack configuration using **babel and JSX** as a default loader, dependencies management.
 
     npm install webpack-react-conf --save-dev
+    
+The configuration contains the following loaders:
+
+* scss-loader (.scss) `node-sass required`
+* file-loader
+* style-loader (css modules)
+* postcss-loader
+* url-loader (jpg and png)
+
+It bundle a file (style.css) with all components that use styles as references.
 
 ## Usage
 
@@ -12,6 +22,7 @@ This module helps to reduce the overloaded webpack configuration using **babel a
     const configuration = require('webpack-react-conf');
     const webpackConf = configuration(
     './src/index.jsx',
+    './build',
     'index.min.js',
     'myAppFoo',
     'umd');
@@ -50,5 +61,9 @@ The module returns a function, it accepts 4 parameters.
 3. The name of the library
 4. The library target
 
+#### Examples
 
-This module extend [webpack-babel-conf](https://github.com/juanpicado/webpack-babel-conf)
+[https://github.com/juanpicado/webpack-examples/tree/master/react-bundle](https://github.com/juanpicado/webpack-examples/tree/master/react-bundle)
+
+
+This module extends [webpack-babel-conf](https://github.com/juanpicado/webpack-babel-conf)
--- webpack.react.conf.js
+++ webpack.react.conf.js
@@ -1,29 +1,27 @@
 var DefaultConfig = require('webpack-babel-conf');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var _ = require('lodash');
 
+var query = { 
+    presets: ['es2015', 'react']
+};
+var styling = ['style', 'css'];
 var reactConf = {
     module: {
-        loaders: [
-          { test: /\.css$/, loader: "style-loader!css-loader" },
-          { test: /\.png$/, loader: "url-loader?limit=100000" },
-          { test: /\.jpg$/, loader: "file-loader" },
-          {
-                test: /\.scss$/,
-                loaders: ['style', 'css', 'sass']
-          },
-          {
-            test: /.jsx?$/,
-            loader: 'babel',
-            exclude: /node_modules/,
-            query: {
-                presets: ['es2015', 'react']
-            }
+        loaders: [          
+          { test: /\.png$/, loader: "url-loader?limit=100000" , exclude: /node_modules/ },
+          { test: /\.jpg$/, loader: "file-loader" , exclude: /node_modules/ },
+          { test: /\.scss$/, loaders: styling.concat(['sass']) , exclude: /node_modules/},
+          { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader') },
+          { test: /.jsx?$/, loader: 'babel', exclude: /node_modules/, query: query
         }
         ]
     },
+    plugins: [
+        new ExtractTextPlugin('style.css', { allChunks: true })
+    ],
     resolve: {
-        modulesDirectories: ['node_modules', 'bower_components'],
-        extensions: ['.css', '.png', '.jpg', '.scss', '.js', '.jsx']
+        modulesDirectories: ['node_modules', 'bower_components']    
     }
 };
 
